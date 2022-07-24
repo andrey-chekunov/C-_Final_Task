1422ee6a963472cb3e6a04b652729157bea5ee34
AndreyChekunov <79117325352@ya.ru>
2022-07-24 20:52:02 +0300

    Add method PrintArray

--- Final_Task/Program.cs
+++ Final_Task/Program.cs
@@ -6,3 +6,14 @@
 // ["hello", "2", "world", ":-)"] -> ["2", ":-)"]
 // ["1234", "1567", "-2", "computer science"] -> ["-2"]
 // ["Russia", "Denmark", "Kazan"] -> []
+
+string[] array = { "hello", "2", "world", ":-)", "1234", "1567", "Rus",
+"-2", "computer science", "Russia", "Denmark", "Kazan", "GB", "GBU" };
+int count = ArrayCheck(array);
+string[] resultArray = FillResultArray(array, count);
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++) Console.Write($"{array[i]}, ");
+    Console.Write($"{array[array.Length - 1]}]");
+}
