ad1deb84c050855c7ff0ec7a3d8dc1d4d986d208
AndreyChekunov <79117325352@ya.ru>
2022-07-24 21:25:00 +0300

    Add diagramm

--- Final_Task/Program.cs
+++ Final_Task/Program.cs
@@ -11,12 +11,6 @@ string[] array = { "hello", "2", "world", ":-)", "1234", "1567", "Rus",
 "-2", "computer science", "Russia", "Denmark", "Kazan", "GB", "GBU" };
 int count = ArrayCheck(array);
 string[] resultArray = FillResultArray(array, count);
-void PrintArray(string[] array)
-{
-    Console.Write("[");
-    for (int i = 0; i < array.Length - 1; i++) Console.Write($"{array[i]}, ");
-    Console.Write($"{array[array.Length - 1]}]");
-}
 int ArrayCheck(string[] array)
 {
     int count = 0;
@@ -38,5 +32,11 @@ string[] FillResultArray(string[] array, int count)
         }
     }
     return result;
+}
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++) Console.Write($"{array[i]}, ");
+    Console.Write($"{array[array.Length - 1]}]");
 }
 PrintArray(resultArray);
